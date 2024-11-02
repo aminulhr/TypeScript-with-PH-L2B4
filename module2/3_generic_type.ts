45ae2644c33654ef0dbfa2c63760006db08337d7
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-02 13:08:36 +0600

    generic type

--- module2/3_generic_type.ts
+++ module2/3_generic_type.ts
@@ -1,36 +1,36 @@
-// {
-//   //
-//   //generic type: string array, number array, boolean array
+{
+  //
+  //generic type: string array, number array, boolean array
 
-//   type genericArray<T> = Array<T>;
+  type genericArray<T> = Array<T>;
 
-//   // const rollNum:number[] =[1,2,3,4];
-//   const rollNum: genericArray<number> = [1, 2, 3, 4];
+  // const rollNum:number[] =[1,2,3,4];
+  const rollNum: genericArray<number> = [1, 2, 3, 4];
 
-//   //   const mentors: string[] = ["Jhanker", "Mir", "Mejbah", "Ali"];
-//   const mentors: genericArray<string> = ["Jhanker", "Mir", "Mejbah", "Ali"];
+  //   const mentors: string[] = ["Jhanker", "Mir", "Mejbah", "Ali"];
+  const mentors: genericArray<string> = ["Jhanker", "Mir", "Mejbah", "Ali"];
 
-//   //   const boolArray:boolean[]=[true,false,true,false];
-//   const boolArray: genericArray<boolean> = [true, false, true, false];
+  //   const boolArray:boolean[]=[true,false,true,false];
+  const boolArray: genericArray<boolean> = [true, false, true, false];
 
-//   //generic object
-//   const user11: genericArray<{ name: string; age: number }> = [
-//     {
-//       name: "rana",
-//       age: 23,
-//     },
-//     {
-//       name: "rakib",
-//       age: 18,
-//     },
-//   ];
+  //generic object
+  const user11: genericArray<{ name: string; age: number }> = [
+    {
+      name: "rana",
+      age: 23,
+    },
+    {
+      name: "rakib",
+      age: 18,
+    },
+  ];
 
-//   //generic tuple
-//   type genericTuple<x, y> = [x, y];
-//   const cupple: genericTuple<string, string> = ["md", "miss"];
-//   const userBio: genericTuple<number, { name: string; email: string }> = [
-//     1234,
-//     { name: "haque", email: "@gmail.com" },
-//   ];
-//   //
-// }
+  //generic tuple
+  type genericTuple<x, y> = [x, y];
+  const cupple: genericTuple<string, string> = ["md", "miss"];
+  const userBio: genericTuple<number, { name: string; email: string }> = [
+    1234,
+    { name: "haque", email: "@gmail.com" },
+  ];
+  //
+}
