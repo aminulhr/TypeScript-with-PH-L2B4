e6549ef2d3b6268d24459a97547127f5786723a0
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-02 22:50:49 +0600

    ts data type

---
+++ module1/11_never.ts
@@ -0,0 +1,5 @@
+//type of never
+function throwError(meg: string) {
+  throw new Error(meg);
+}
+throwError("mushkil se error hogaya");
--- module1/dist/4spreadOparatoe.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const moreStudents = ["Rakib", "Hasan", "munna", "sozib", "babu", "abul"];
-const [...rest] = moreStudents; //use spread operator  '...rest'
-console.log(rest);
