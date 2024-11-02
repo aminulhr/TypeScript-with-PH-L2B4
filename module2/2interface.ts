96d2b128f85e6b020341ddf18491232a40a9e5bf
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-02 13:07:25 +0600

    interface

--- module2/2interface.ts
+++ module2/2interface.ts
@@ -1,1 +1,39 @@
-// সকল premitive data  তে type alias ব্যবহার করবো। আর object এর ক্ষেত্রে  type alias  ও ব্যবহার করতে পারি আবার interface  ও ব্যবহার করতে পারি।
+{
+  // সকল premitive data  তে type alias ব্যবহার করবো। আর object এর ক্ষেত্রে  type alias  ও ব্যবহার করতে পারি আবার interface  ও ব্যবহার করতে পারি।
+  type user1 = {
+    name: string;
+    age: number;
+  };
+  interface user2 {
+    name: string;
+    age: number;
+  }
+  type userWithRoll = user1 & { roll: string };
+  interface userWithRoll2 extends user2 {
+    roll: number;
+  }
+  const user1: userWithRoll = {
+    name: "rana",
+    age: 23,
+    roll: "509",
+  };
+  const userWithRoll2: userWithRoll2 = {
+    name: "rana",
+    age: 23,
+    roll: 123,
+  };
+
+  //array declaker by interface
+  interface roll2 {
+    [index: number]: number;
+  }
+  const rollNumber: roll2 = [1, 2, 3];
+
+  // interface in function
+  type Add = (num1: number, num2: number) => number; //alias type
+  interface Add2 {
+    (num1: number, num2: number): number;
+  }
+
+  const add: Add2 = (num1, num2) => num1 + num2;
+}
---
+++ module2/4_generic_by_interface.ts
@@ -0,0 +1,48 @@
+// {
+//   //generic with interface
+//   interface Developer<T> {
+//     name: string;
+//     computer: {
+//       brand: string;
+//       model: string;
+//       releaseYear: number;
+//     };
+//     smartWatch: T;
+//   }
+//   const poorDeveloper: Developer<{
+//     brand: string;
+//     model: string;
+//     disply: string;
+//   }> = {
+//     name: "Rana",
+//     computer: {
+//       brand: "HP",
+//       model: "Elitebook",
+//       releaseYear: 2013,
+//     },
+//     smartWatch: {
+//       brand: "casic",
+//       model: "C100",
+//       disply: "oled",
+//     },
+//   };
+//   const richDeveloper: Developer<{
+//     brand: string;
+//     model: string;
+//     palsRate: boolean;
+//     sleepTrack: boolean;
+//   }> = {
+//     name: "rakib",
+//     computer: {
+//       brand: "makbook",
+//       model: "pro 2",
+//       releaseYear: 2024,
+//     },
+//     smartWatch: {
+//       brand: "apple",
+//       model: "m1",
+//       palsRate: true,
+//       sleepTrack: true,
+//     },
+//   };
+// }
